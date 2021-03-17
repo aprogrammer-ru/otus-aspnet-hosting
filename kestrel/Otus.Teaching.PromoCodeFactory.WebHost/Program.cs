050aa9599afd004cd3b2f67e8107fb45593fc930
Alexander Novikov <alexander.novicov@gmail.com>
2021-03-17 15:15:05 +0100

    added kestrel

---
+++ kestrel/Otus.Teaching.PromoCodeFactory.WebHost/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Otus.Teaching.PromoCodeFactory.WebHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.ConfigureKestrel(options =>
+                        {
+                            options.Listen(IPAddress.Loopback, 5026);
+                        });
+                        webBuilder.UseStartup<Startup>();
+                    }
+                    );
+    }
+}
